db1336b623aa24208e8c42988b99cf2482506f0a
Luiz-Edu0202 <Brayner.luizeduardo@gmail.com>
2022-10-17 10:41:13 -0300

    Atualização de Readme

--- PyFinanceiro.py
+++ PyFinanceiro.py
@@ -6,12 +6,38 @@ def leitor_de_arquivos():
     return lista_de_dados
 
 
+def identificador_de_valores(lista_de_dados):
+    valores = []
+    for valor in lista_de_dados:
+        valores.append(int(valor.split(",")[1]))   
+    return valores
+
+            
+def organizador_de_texto(lista_de_dados,meses,valor_liquido,media_das_mudancas,maior_aumento,maior_diminuicao):
+    
+    
+    
+    mensagem = f"""
+    Analise financeira
+    ----------------------------
+    Total de meses: {meses}
+    Total: $ {valor_liquido:.2f}
+    Média: $ {media:.2f}
+    Variação da média: $ {media_das_mudancas:.2fs}
+    Maior aumento nos lucros: fevereiro de 2012 ($ {maior_aumento:.2f})
+    Maior redução nos lucros: setembro de 2013 ($ {maior_diminuicao:.2f})
+    """
+    print(mensagem)
+    return mensagem
+        
+
 def contador_de_meses(lista_de_dados):
     numero_total_de_meses = 0
     meses = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec"
     for linha in lista_de_dados:
         if linha.split("-")[0] in meses: numero_total_de_meses += 1
     return numero_total_de_meses
+
 
 
 def calculador_de_valor_liquido(lista_de_dados):
@@ -25,21 +51,56 @@ def media_de_lucros_perdas(valor_liquido,meses):
     return valor_liquido / meses
 
 
-def media_das_mudancas(media,lista_de_dados):
+def maior_aumento_menor_reducao(lista_de_dados,):
+    valores = identificador_de_valores(lista_de_dados)
+    maior_aumento = valores[0] - valores[1]
+    maior_diminuicao = valores[0] - valores[1]
+    valor_anterior = valores[0]
+    for valor in valores:
+        if valor_anterior - valor < maior_aumento:
+            maior_aumento = valor_anterior - valor
+        elif valor_anterior - valor > maior_diminuicao:
+            maior_diminuicao = valor_anterior - valor
+        valor_anterior = valor
+    return maior_aumento, maior_diminuicao
+
+
+def media_das_mudancas(lista_de_dados):
     media_das_mudancas = 0
-    for valor in lista_de_dados:
-        mes = int(valor.split(",")[1])   
-        media_das_mudancas += mes - media
+    valores = identificador_de_valores(lista_de_dados)
+    valor_anterior = valores[0]
+    for valor in valores:
+        media_das_mudancas += valor_anterior - valor
+    media_das_mudancas = media_das_mudancas/ (len(valores)) 
     return media_das_mudancas
 
 
-def maior_aumento_menor_reducao(lista_de_dados):
-    return max(lista_de_dados), min(lista_de_dados)
 
-
+    
 lista_de_dados = leitor_de_arquivos()
 meses = contador_de_meses(lista_de_dados)
 valor_liquido = calculador_de_valor_liquido(lista_de_dados)
 media = media_de_lucros_perdas(valor_liquido,meses)
-media_das_mudancas = media_das_mudancas(media,lista_de_dados)
-print(maior_aumento_menor_reducao(lista_de_dados))
+media_das_mudancas = media_das_mudancas(lista_de_dados)
+maior_aumento,maior_diminuicao = maior_aumento_menor_reducao(lista_de_dados)
+media_das_mudancas(lista_de_dados)
+
+
+"""
+O número total de meses incluídos no conjunto de dados ok
+O valor total líquido de "Lucros / Perdas" durante todo o período ok
+A média dos "Lucros / Perdas" durante todo o período ok
+A média das mudanças em "Lucros / Perdas" durante todo o período
+O maior aumento nos lucros (data e valor) durante todo o período ok
+A maior redução nas perdas (data e valor) ao longo de todo o período ok
+"""
+"""
+Analise financeira
+----------------------------
+Total de meses: 86
+Total: $ 38382578
+Média: $ 446309,04
+Variação da média: $ -2315,12
+Maior aumento nos lucros: fevereiro de 2012 ($ 1926159)
+Maior redução nos lucros: setembro de 2013 ($ -2196167)
+"""
--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# interview_tests
+## interview_tests
 some questions of python for my interviews
+
+## 💸 PyFinanceiro
+
+* Neste desafio, você tem a tarefa de criar um script Python para analisar os registros financeiros de sua empresa. Você fornecerá um conjunto de dados financeiros chamado [dados_financeiros.txt](https://drive.google.com/file/d/1g4A0DkMdGxwv9JSGO32DYqvPby45pIHG/view?usp=sharing). O conjunto de dados é composto por duas colunas: `Data` e `Lucros/Perdas`, separados por virgula. (Felizmente, sua empresa tem padrões bastante flexíveis para a contabilidade, então os registros são simples.)
+
+* Sua tarefa é criar um script Python que analise os registros para calcular cada um das seguintes informações:
+
+  * O número total de meses incluídos no conjunto de dados
+
+  * O valor total líquido de "Lucros / Perdas" durante todo o período
+
+  * A média dos "Lucros / Perdas" durante todo o período
+
+  * A média das mudanças em "Lucros / Perdas" durante todo o período
+
+  * O maior aumento nos lucros (data e valor) durante todo o período
+
+  * A maior redução nas perdas (data e valor) ao longo de todo o período
+
+* Por exemplo, sua análise deve ser semelhante a esta abaixo:
+
+``` text
+Analise financeira
+----------------------------
+Total de meses: 86
+Total: $ 38382578
+Média: $ 446309,04
+Variação da média: $ -2315,12
+Maior aumento nos lucros: fevereiro de 2012 ($ 1926159)
+Maior redução nos lucros: setembro de 2013 ($ -2196167)
+```
+
+* Além disso, seu script final deve imprimir a análise no terminal e exportar um arquivo de texto `relatório.txt` com os resultados.
+
+## 🗳️ PyVotação
+
+* Neste desafio, você tem a tarefa de ajudar uma pequena cidade rural a modernizar seu processo de contagem de votos. (Até agora, o tio Cleiton vinha contando-os um por um com confiança, mas, infelizmente, sua concentração não é o que costumava ser.)
+
+* Você receberá um conjunto de dados de enquete chamado [dados_eleção.txt](https://drive.google.com/file/d/13tarkZMlfvMcHnS8K49pdF-GMYOwgggg/view?usp=sharing). O conjunto de dados é composto por três colunas: `ID do eleitor`,` Município` e `Candidato`. Sua tarefa é criar um script Python que analise os votos e calcule cada uma das seguintes informações:
+
+  * O número total de votos expressos
+
+  * Uma lista completa de candidatos que receberam votos
+
+  * A porcentagem de votos que cada candidato ganhou
+
+  * O número total de votos que cada candidato ganhou
+
+  * O vencedor da eleição com base no voto popular.
+
+* Por exemplo, sua análise deve ser semelhante a esta abaixo:
+
+  ```text
+  Resultados eleitorais
+  -------------------------
+  Total de votos: 3521001
+  -------------------------
+  Khan: 63.0% (2218231)
+  Correy: 20.0% (704200)
+  Li: 14.0% (492940)
+  O'Tooley: 3.0% (105630)
+  -------------------------
+  Vencedor: Khan
+  -------------------------
+  ```
+
+* Além disso, seu script final deve imprimir a análise no terminal e exportar um arquivo de texto `resultado.txt` com os resultados.
